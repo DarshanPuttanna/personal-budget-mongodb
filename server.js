5e1dc5544f16b2dbedf2d67dd3cc1f2b232bf19c
Darshan P <darshan.p.darshi264@gmail.com>
2020-10-04 15:22:08 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,39 +1,43 @@
 const express = require ('express');
 const app = express();
 const port= 3000; 
+const dataPath = require ('./budgetData.json');
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat Out',
-        budget : 25
-    },
+// const budget = {
+//     myBudget: [
+//     {
+//         title: 'Eat Out',
+//         budget : 25
+//     },
 
-    {
-        title: 'Rent',
-        budget : 375
-    },
+//     {
+//         title: 'Rent',
+//         budget : 375
+//     },
 
-    {
-        title: 'Grocery',
-        budget : 110
-    },
-]};
+//     {
+//         title: 'Grocery',
+//         budget : 110
+//     },
+// ]};
 
 app.get('/hello', (req, res) =>{
     res.send('Hello WOrld');
 });
 
 app.get('/budget', (req, res) =>{
-    res.json(budget);
+    res.json(dataPath);
+
 });
-
-
-
-
 
 app.listen(port, () =>{
 console.log('Example app listening at http://localhost:${port}')
 });
+
+ 
+ 
+
+
+
